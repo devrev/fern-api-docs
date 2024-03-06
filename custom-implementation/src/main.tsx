df7412e06241fcfe802c0d7d9d58e43c760da4dc
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-03-06 17:42:39 +0530

    fix: header data rendering

--- custom-implementation/src/main.tsx
+++ custom-implementation/src/main.tsx
@@ -26,9 +26,9 @@ const render = async () => {
 let observations = 0
 document.addEventListener('DOMContentLoaded', async () => {
   console.log('DOMContentLoaded')
-  void (await render())
+  await render()
   new MutationObserver(async (e, o) => {
-    void (await render())
+    await render()
     for (const item of e) {
       if (item.target instanceof HTMLElement) {
         const target = item.target

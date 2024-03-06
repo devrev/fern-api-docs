eb54c6631ce7a60b2004ec8ea0dbea85d5f036c5
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-03-06 17:57:08 +0530

    fix: header data rendering

--- custom-implementation/src/main.tsx
+++ custom-implementation/src/main.tsx
@@ -8,27 +8,27 @@ import React from 'react'
 import Header from './components/header'
 import Footer from './components/footer'
 
-import { getPageData } from './modules/sanity/utils'
+// import { getPageData } from './modules/sanity/utils'
 
-const render = async () => {
-  const data = await getPageData()
+const render = () => {
+  // const data = await getPageData()
 
   ReactDOM.render(
-    React.createElement(Header, { ...data?.header }),
+    React.createElement(Header),
     document.getElementById('fern-header'),
   )
   ReactDOM.render(
-    React.createElement(Footer, { ...data?.footer }),
+    React.createElement(Footer),
     document.getElementById('fern-footer'),
   )
 }
 
 let observations = 0
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded')
-  await render()
+  render()
   new MutationObserver(async (e, o) => {
-    await render()
+    render()
     for (const item of e) {
       if (item.target instanceof HTMLElement) {
         const target = item.target

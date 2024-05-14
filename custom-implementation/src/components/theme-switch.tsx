bd284ddcc7965f9a4666364cb56f2392807affb9
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-05-14 09:08:28 +0530

    fix: theme switch

--- custom-implementation/src/components/theme-switch.tsx
+++ custom-implementation/src/components/theme-switch.tsx
@@ -2,19 +2,6 @@ import React, { useEffect } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-const items = [
-  {
-    name: 'public',
-    title: 'Public',
-    url: '/about/for-developers',
-  },
-  {
-    name: 'beta',
-    title: 'Beta',
-    url: '/beta/about/for-developers',
-  },
-]
-
 export const ThemeSwitch = () => {
   const [selected, setSelected] = React.useState('public')
 
@@ -69,32 +56,59 @@ export const ThemeSwitch = () => {
             <DropdownMenu.RadioGroup
               value={selected}
               onValueChange={setSelected}>
-              {items?.map((item) => (
-                <DropdownMenu.RadioItem value={item.name}>
-                  <a className="fern-dropdown-item" href={item.url}>
-                    <span
-                      style={{
-                        marginRight: 'auto',
-                      }}>
-                      <span>{item.title}</span>
-                    </span>
-                    <DropdownMenu.ItemIndicator className="fern-dropdown-item-indicator">
-                      <svg
-                        width="15"
-                        height="15"
-                        viewBox="0 0 15 15"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
-                          fill="currentColor"
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"></path>
-                      </svg>
-                    </DropdownMenu.ItemIndicator>
-                  </a>
-                </DropdownMenu.RadioItem>
-              ))}
+              <DropdownMenu.RadioItem value="public">
+                <a className="fern-dropdown-item" href="/about/for-developers">
+                  <span
+                    style={{
+                      marginRight: 'auto',
+                    }}>
+                    <span>Public</span>
+                  </span>
+                  <DropdownMenu.ItemIndicator className="fern-dropdown-item-indicator">
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"></path>
+                    </svg>
+                  </DropdownMenu.ItemIndicator>
+                </a>
+              </DropdownMenu.RadioItem>
+              <DropdownMenu.RadioItem value="beta">
+                <a
+                  className="fern-dropdown-item"
+                  style={{
+                    justifyContent: 'space-between',
+                  }}
+                  href="/beta/about/for-developers">
+                  <span
+                    style={{
+                      marginRight: 'auto',
+                    }}>
+                    <span>Beta</span>
+                  </span>
+                  <DropdownMenu.ItemIndicator className="fern-dropdown-item-indicator">
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"></path>
+                    </svg>
+                  </DropdownMenu.ItemIndicator>
+                </a>
+              </DropdownMenu.RadioItem>
             </DropdownMenu.RadioGroup>
           </DropdownMenu.Content>
         </DropdownMenu.Portal>

6295110498df8d8542744be6bd2f802121af3d2f
Nikhil <54956597+nikhiltidke101@users.noreply.github.com>
2024-05-14 10:16:05 +0530

    add: custom navigation added (#9)
    * add: custom navigation added
    
    * add: github workflow added
    
    * add: github workflow added
    
    * update: docs yaml, and preview workflow
    
    * fix: preview workflow
    
    * fix: header footer data fething
    
    * fix: msc bump
    
    * add: github auth token added
    
    * fix: auth token
    
    * fix: github workflow
    
    * fix: github workflow
    
    * rerun workflow
    
    * remove: npm rc
    
    * fix: github workflow
    
    * fix: github workflow v2
    
    * fix: github workflow v3
    
    * fix: github workflow v4
    
    * fix: github workflow v5
    
    * fix: fetch call
    
    * fix: header and footer cjs added
    
    * fix: header and footer cjs added
    
    * fix: header data rendering
    
    * fix: header data rendering
    
    * fix: header data rendering
    
    * fix: header data rendering
    
    * fix: header data rendering
    
    * add: comment asyn data call
    
    * fix: page data sanity fix
    
    * fix: page data sanity fix
    
    * fix: page data sanity fix
    
    * header fix
    
    * header fix
    
    * fix: workflows
    
    * bump: msc version
    
    * fix: workflows
    
    * fix: workflows
    
    * fix: workflows
    
    * fix: update header
    
    * fix: update header
    
    * fix: update header
    
    * add: plug script
    
    * add: plug script
    
    * add: plug script
    
    * add: plug script
    
    * add: plug script
    
    * add: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * remove: plug script
    
    * fix: css problem
    
    * fix: css problem
    
    * fix: css problem
    
    * fix: css problem
    
    * fix: footer css
    
    * fix: sidenav sibling
    
    * fix: sidenav sibling
    
    * fix: sidenav sibling
    
    * fix: sidenav sibling seperated
    
    * fix: sidenav sibling seperated v2
    
    * fix: sidenav sibling seperated v3
    
    * fix: sidenav sibling seperated v3
    
    * fix: sidenav sibling seperated v4
    
    * fix: sidenav sibling seperated v5
    
    * add: api toggles
    
    * add: api toggles
    
    * add: api toggles v2
    
    * fix: build
    
    * add: fern styles
    
    * fix: rendering
    
    * fix: rendering
    
    * fix: rendering v2
    
    * fix: styles on dropdown
    
    * fix: dropdown border
    
    * fix: theme switch
    
    * fix: theme switch
    
    * revert: to stable build
    
    * fix: code fixes
    
    * revert: to stable build

---
+++ custom-implementation/src/components/header.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import {
+  ComponentConfigContext,
+  HeaderV30 as SharedHeader,
+} from '@devrev/marketing-shared-components/dist/cjs'
+
+import './header.css'
+
+const Header = ({
+  logo,
+  links,
+  actions,
+  version,
+  collapseOnScroll = true,
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+any) => {
+  return (
+    <div>
+      <ComponentConfigContext.Provider
+        value={{
+          origin: 'https://api.docs.devrev.ai',
+        }}>
+        <SharedHeader
+          logo={logo}
+          items={links}
+          actions={actions}
+          version={version}
+          collapseOnScroll={collapseOnScroll}
+          wrapperClassName="header"
+        />
+      </ComponentConfigContext.Provider>
+    </div>
+  )
+}
+
+export default Header
---
+++ custom-implementation/src/modules/sanity/utils.ts
@@ -0,0 +1,32 @@
+import { createClient } from '@sanity/client'
+import { FOOTER_V3_SCHEMA, HEADER_V3_SCHEMA } from './schema'
+
+export const CLIENT = createClient({
+  projectId: 'umrbtih2',
+  dataset: 'production',
+  apiVersion: '2022-03-25',
+  useCdn: true,
+})
+
+export const SANITY_TYPES = {
+  PAGE_CONTENT: 'pageContent',
+} as const
+
+export const getPageData = async (): Promise<{
+  header: object
+  footer: object
+}> => {
+  const header = await CLIENT.fetch(
+    `*[_type == 'headerV2' && slug.current == 'developer']{
+        ${HEADER_V3_SCHEMA}
+      }[0]`,
+  )
+
+  const footer = await CLIENT.fetch(
+    `*[_type == 'footerV2' && slug.current == 'default']{
+        ${FOOTER_V3_SCHEMA}
+      }[0]`,
+  )
+
+  return { header, footer }
+}

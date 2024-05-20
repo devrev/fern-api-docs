73bb5eb54dc61ce6c57c4dbb80d72068aaa61d03
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-05-20 19:11:36 +0530

    fix: navigation on mobile v9

--- custom-implementation/src/main.tsx
+++ custom-implementation/src/main.tsx
@@ -19,6 +19,7 @@ const render = async () => {
   const data = await getPageData()
   const sidenav = document.querySelector('button.fern-search-bar')
     ?.parentElement as HTMLElement
+
   const theme = document.getElementsByTagName('html')[0].getAttribute('class')
 
   if (!document.getElementById('theme-switch')) {

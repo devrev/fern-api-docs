c7dee6a1c0d3c6ba6d35382f679437af5f7f2598
Karuna tata <karunatata63@gmail.com>
2024-05-27 14:43:59 +0530

    Revert "theme"
    This reverts commit a65a5625e394392481131bb235464fce58f118dc.

--- custom-implementation/src/components/header.tsx
+++ custom-implementation/src/components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import {
   ComponentConfigContext,
   HeaderV30 as SharedHeader,
@@ -16,17 +16,10 @@ const Header = ({
 }: // eslint-disable-next-line @typescript-eslint/no-explicit-any
 any) => {
   const html = document.getElementsByTagName('html')[0]
-  const [theme, setTheme] = useState(html.getAttribute('class') || 'light') // Default to 'light' if no class is present
-
-  const themeRef = useRef(theme)
-  themeRef.current = theme
-
+  const [theme, setTheme] = useState(html.getAttribute('class'))
   useEffect(() => {
     const observer = new MutationObserver(() => {
-      const newTheme = html.getAttribute('class') || 'light'
-      if (themeRef.current !== newTheme) {
-        setTheme(newTheme)
-      }
+      setTheme(html.getAttribute('class'))
     })
 
     const config = { attributes: true, attributeFilter: ['class'] }
@@ -46,7 +39,7 @@ any) => {
           logo={logo}
           items={links}
           actions={actions}
-          version={theme === 'dark' ? 'dark' : 'light'} // Ensure the theme is correctly reflected
+          version={theme === 'dark' ? 'light' : 'dark'}
           collapseOnScroll={collapseOnScroll}
           wrapperClassName="custom-header"
         />

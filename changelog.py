1b4356c2a9fc2748af53c210c0a664848b6a98a8
Ben Colborn <ben.colborn@devrev.ai>
2025-01-03 13:23:05 -0800

    construct links

--- changelog.py
+++ changelog.py
@@ -1,23 +1,19 @@
 import yaml
 import argparse
 
-def main(args):
+def main(vrn):
 
-    if args.beta:
-        spec_src = './fern/apis/beta/openapi-beta.yaml'
-
-    get_specs(spec_src) 
-
-def get_specs(src):
+    src = f"./fern/apis/{vrn}/openapi-beta.yaml"
 
     with open(src, 'r') as s:
         spec = yaml.safe_load(s)
-    apis = {}
+        
     for endp, defn in spec['paths'].items():
-        apis[endp] = {'tags': defn.get('tags')}
         for prop, desc in defn.items():
-            apis[endp] = {prop: desc.get('operationId')}
-    print(apis)
+            tag = desc.get('tags')[0]
+            opId = desc.get('operationId')
+        link = f"- [{tag} > {endp.split('.')[-1]} `{opId}`](https://developer.devrev.ai/{vrn}/api-reference/{tag}/{opId})"
+        print(link)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate release notes")
@@ -26,7 +22,12 @@ if __name__ == "__main__":
     parser.add_argument('--public', default=False,
                         action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
-    if (args.beta or args.public):
-        main(args)
+    version = None
+    if args.beta:
+        version = 'beta'
+    elif args.public:
+        version = 'public'
     else:
         print("Specify --beta, --public, or both.")
+    if version:
+        main(version)

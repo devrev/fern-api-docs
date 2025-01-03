bbb3d72e81d97f3b670c19492d13bec97776017c
Ben Colborn <ben.colborn@devrev.ai>
2025-01-03 12:45:35 -0800

    Revert "get paths from sitemap"
    sitemap is not complete
    
    This reverts commit 6963d274cc6b57ce117bf4af3b0912cd08617a87.

--- changelog.py
+++ changelog.py
@@ -1,34 +1,14 @@
 import yaml
 import argparse
-import urllib.request
-import xmltodict
-
 
 def main(args):
 
-    with urllib.request.urlopen('http://developer.devrev.ai/sitemap.xml') as f:
-        xml_data = f.read().decode('utf-8')
-
     if args.beta:
-        version = 'beta'
-    elif args.public:
-        version = 'public'
+        spec_src = './fern/apis/beta/openapi-beta.yaml'
 
-    data_dict = xmltodict.parse(xml_data)
-    urls = []
-    for i in data_dict['urlset']['url']:
-        loc = i['loc'].replace('https://developer.devrev.ai', '')
-        if '/api-reference' in loc and loc.startswith(f"/{version}"):
-            path = loc.split('/')
-            link = f"- [{path[-2]} > {path[-1]}]({loc})"
-            print(link)
+    get_specs(spec_src) 
 
-
-    #get_specs(version) 
-
-def get_specs(version):
-
-    src = f"./fern/apis/{version}/openapi-beta.yaml"
+def get_specs(src):
 
     with open(src, 'r') as s:
         spec = yaml.safe_load(s)

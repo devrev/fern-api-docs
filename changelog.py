d3f6926bb56b6df0354a3dcee7fbf952aea6e31a
Ben Colborn <ben.colborn@devrev.ai>
2025-01-09 14:49:35 -0800

    run changelog script and commit final output

--- changelog.py
+++ changelog.py
@@ -10,17 +10,16 @@ def main(vrn, d):
 
     print(f"Generating prompt for `{vrn}` API on {d}.")
 
-    p = gen_prompt(f"temp/{vrn}/{d}_oasdiff.md", get_links(vrn), )
+    p = gen_prompt(f"temp/{vrn}/{d}_oasdiff.md", get_links(vrn), vrn)
 
     pr_file = f"temp/{vrn}/{d}_prompt.md"
     with open(pr_file, 'w', encoding="utf-8") as outfile:
         outfile.write(p)
         print(f"Wrote prompt to {pr_file}.")
 
-    l = f"# {d}\n\n"
-    l += gen_log(p)
+    l = gen_log(p)
 
-    log_file = f"./fern/apis/{vrn}/{d}.md"
+    log_file = f"./fern/apis/{vrn}/changelog/{d}.md"
     with open(log_file, 'w', encoding="utf-8") as outfile:
         outfile.write(l)
         print(f"Wrote log to {log_file}.")
@@ -28,6 +27,7 @@ def main(vrn, d):
 
 def gen_log(prompt):
 
+    log = None
     auth = os.environ.get('LLM_JWT')
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {auth}"}
     payload = {
@@ -40,19 +40,24 @@ def gen_log(prompt):
         ]
     }
 
-    r = requests.post('https://openwebui.dev.devrev-eng.ai/api/chat/completions', json=payload,
-                        headers=headers)
-    log = r.json()['choices'][0]['message']['content']
-    log = re.sub(r"^Here's.*\n?", '', log, flags=re.MULTILINE)
+    try:
+        r = requests.post('https://openwebui.dev.devrev-eng.ai/api/chat/completions', json=payload,
+                            headers=headers)
+        log = r.json()['choices'][0]['message']['content']
+        log = re.sub(r"^Here's.*\n?", '', log, flags=re.MULTILINE)
+        log = re.sub(r"^Let me know.*\n?", '', log, flags=re.MULTILINE)
+    except Exception as e:
+        print(
+            f"Failed to generate changelog. Error: {type(e)} {e} {r}")
     return log
 
 
-def gen_prompt(oasdiff, links):
+def gen_prompt(oasdiff, links, version):
     with open(oasdiff, 'r') as infile:
         oasdiff = infile.read()
 
     prompt = f"""
-Please provide an API changelog from the following OASDiff of OpenAPI spec changes. The output should be in markdown format grouping endpoints by use case/object type. For cases where some schema is modified, please also tell what endpoints it affects. Wherever an endpoint, property, or enum value is mentioned, surround it with backticks (`). Wherever an API is mentioned, include a link to the API reference from the list of API links in markdown.
+Please provide an API changelog for the {version} API from the following OASDiff of OpenAPI spec changes. The output should be in markdown format grouping endpoints by use case/object type. For cases where some schema is modified, please also tell what endpoints it affects. Wherever an endpoint, property, or enum value is mentioned, surround it with backticks (`). Wherever an API is mentioned, include a link to the {version} API reference from the list of {version} API links in markdown.
 
 <oasdiff>
 {oasdiff}
@@ -84,11 +89,11 @@ def get_links(version):
                 apis[tag][endp] = api
 
     md = f"# {version}\n\n"
-    for tag, api in apis.items():
+    for tag, api in sorted(apis.items()):
         md += f"\n## {tag}\n\n"
-        for val in api.values():
-            link_text = f"{tag} > {val['method']} `{val['opId']}`"
-            md += f"- [{link_text}]({val['target']})\n"
+        for endp, val in api.items():
+            link_text = f"**{tag} > {val['method']}**: `{val['opId']}`"
+            md += f"- [{endp}]({val['target']}) {link_text}\n"
     return md
 
 if __name__ == "__main__":

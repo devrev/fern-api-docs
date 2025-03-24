ed3abf5c9fc0f3e8b73ecd613dbf08f0bc0f01c0
Ben Colborn <ben.colborn@devrev.ai>
2025-03-24 15:00:52 -0700

    refactor

--- stylecheck.py
+++ stylecheck.py
@@ -6,7 +6,7 @@ import llm_client
 
 import difflib
 
-def generate_git_diff(before, after, filename="file.txt"):
+def gen_diff(before, after, filename="file.txt"):
 
     before_lines = before.splitlines(keepends=True)
     after_lines = after.splitlines(keepends=True)
@@ -19,21 +19,7 @@ def generate_git_diff(before, after, filename="file.txt"):
     )
     return "".join(diff)
 
-def my_writer(content, file, note=None):
-    if (content):
-        with open(file, 'w', encoding="utf-8") as outfile:
-            outfile.write(content)
-            print(' '.join(['Wrote', note, 'to', file]))
-            #print(f"Wrote {note} to {file}.")
-    else:
-        print(f"Failed to write {file}.")
-
-def main(args):
-    print(f"Checking style for {args.doc}")
-    doc_name, ext = os.path.splitext(os.path.basename(args.doc))
-
-    with open(args.doc, 'r', encoding="utf-8") as infile:
-        doc = infile.read()
+def gen_prompt(args):
 
     with open('style/prompt.md', 'r') as infile:
         prompt = infile.read()
@@ -58,15 +44,34 @@ def main(args):
     prompt += "\n</terminology>\n\n"
 
     prompt += "\n\n<document>\n\n"
-    prompt += doc
+    prompt += args.doc
     prompt += "\n\n</document>\n\n"
-    
+
+    return prompt
+
+def my_writer(content, file, note=None):
+    if (content):
+        with open(file, 'w', encoding="utf-8") as outfile:
+            outfile.write(content)
+            print(' '.join(['Wrote', note, 'to', file]))
+            #print(f"Wrote {note} to {file}.")
+    else:
+        print(f"Failed to write {file}.")
+
+def main(args):
+    print(f"Checking style for {args.doc}")
+    doc_name, ext = os.path.splitext(os.path.basename(args.doc))
+
+    with open(args.doc, 'r', encoding="utf-8") as infile:
+        args.doc = infile.read()
+
+    prompt = gen_prompt(args)
     my_writer(prompt, f"temp/{doc_name}_prompt.md", 'prompt')
     response = llm_client.get_response(prompt)
     my_writer(response, f"temp/{doc_name}_response.md", 'response')
     revision = llm_client.get_lines_between_tags(response, 'document')
     my_writer(revision, f"temp/{doc_name}_revision{ext}", 'revision')
-    diff = generate_git_diff(doc, revision, doc_name)
+    diff = gen_diff(args.doc, revision, doc_name)
     my_writer(diff, f"temp/{doc_name}.diff", 'diff')
 
 if __name__ == "__main__":

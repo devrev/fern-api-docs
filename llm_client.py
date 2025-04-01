041ec80b2fa6a55c86513b9eb31c8a9b58b5b56f
Ben Colborn <117322957+bc-devrev@users.noreply.github.com>
2025-04-01 08:51:16 -0700

    check style (#173)
    * add instructions for local instance
    
    * generate prompt
    
    * refactored for reuse and graceful failure
    
    * send to LLM
    
    * extract revision from element
    
    * specify additional styles
    
    * directory name
    
    * terminology
    
    * refactored
    
    * get diff between original and revision
    
    * terminology violations
    
    * US English
    
    * refactor
    
    * improve diff
    
    * option to skip LLM for testing
    
    * get the title back
    
    * parse response and diff into comments
    
    * parse response and diff into comments
    
    * fix line calculation
    
    * fix line calculation
    
    * post overall comment

---
+++ llm_client.py
@@ -0,0 +1,40 @@
+import os
+import re
+import requests
+
+def get_response(prompt):
+
+    auth = os.environ.get('LLM_JWT')
+    if auth:
+        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {auth}"}
+        payload = {
+            "model": "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        }
+
+        try:
+            r = requests.post('https://openwebui.dev.devrev-eng.ai/api/chat/completions', json=payload,
+                                headers=headers)
+            response = r.json()['choices'][0]['message']['content']
+            response = re.sub(r"^# .*\n?", '', response, flags=re.MULTILINE)
+            response = re.sub(r"^Here.*\n?", '', response, flags=re.MULTILINE)
+            response = re.sub(r"^Let me know.*\n?", '', response, flags=re.MULTILINE)
+            return response
+        except Exception as e:
+            print(f"Failed to generate changelog. Error: {type(e)} {e} {r}")
+            return None
+        
+def get_lines_between_tags(text, tag):
+  pattern = r'<' + tag + r'>(.*?)<\/' + tag + r'>'
+  matches = re.findall(pattern, text, re.DOTALL)
+  return "".join([match.strip() for match in matches])
+
+def get_lines_before_tag(text, tag):
+  pattern = r'(.*?)<' + tag + r'>'
+  matches = re.findall(pattern, text, re.DOTALL)
+  return "".join([match.strip() for match in matches])

35de9ea2bb2304c4cd930104faf2f28d842b30b7
Nikhil Tidke <nikhil.st8@gmail.com>
2025-05-14 13:25:11 +0530

    refactor: integrate search component rendering into main application

--- custom-implementation/src/main.tsx
+++ custom-implementation/src/main.tsx
@@ -29,11 +29,16 @@ const render = async () => {
 
   const theme = document.getElementsByTagName('html')[0].getAttribute('class')
 
-  if (!document.getElementById('theme-switch')) {
+  if (!document.getElementById('theme-switch') && !document.getElementById('search-component')) {
     const wrapper = document.createElement('div')
     wrapper.setAttribute('id', 'theme-switch')
     sidenav.appendChild(wrapper)
     ReactDOM.render(React.createElement(ThemeSwitch), wrapper)
+
+    const search = document.createElement('div')
+    search.setAttribute('id', 'search-component')
+    sidenav.appendChild(search)
+    ReactDOM.render(React.createElement(Search), search)
   }
 
   // if(!document.getElementById('search-component-wrapper')) {
